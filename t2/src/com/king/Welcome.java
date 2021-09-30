0697879a53eef3d4c1285ba07b8a90a2a5643baf
laksys linux <laksys@gmail.com>
2021-09-30 20:34:44 +0530

    main method was added

--- t2/src/com/king/Welcome.java
+++ t2/src/com/king/Welcome.java
@@ -1,5 +1,10 @@
 package com.king;
 
 public class Welcome {
-
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			System.out.println("this is welcome message");
+			System.out.println("create by me...");
+		}
+	}
 }
